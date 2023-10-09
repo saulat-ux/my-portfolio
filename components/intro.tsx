b34472d021e2991bd9afbaefe839d1c95721ce9f
saulat-ux <saulat.zubair@gmail.com>
2023-10-09 22:36:47 +0500

    working on projects

--- components/intro.tsx
+++ components/intro.tsx
@@ -40,19 +40,19 @@ const Intro = () => {
                 </div>
         </div>
 
-        <motion.p className='mb-10 mt-4 text-2xl font-medium 
+        <motion.h1 className='mb-10 mt-4 text-2xl font-medium 
         sm:text-4xl'
         initial= {{ opacity:0, y:100}}
         animate = {{opacity:1, y:0 }}
         >
-        <span className="font-bold">Hello, I'm Saulat.</span> I'm a{" "}
+        <span className="font-bold text-blue-800">Hi, I'm Saulat Zubair.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">couple of years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>.My side hobbies are{" "}
         <span className="font-bold">Tabletennis and kickboxing</span>.
          My focus is{" "}
         <span className="underline">React (Node.js , Next.js)</span>.
-    </motion.p>
+    </motion.h1>
     <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 
       px-4 text-lg font-medium'
       initial = {{opacity: 0 , y:100}}
